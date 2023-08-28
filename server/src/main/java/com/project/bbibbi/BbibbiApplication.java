eb11b858de0a8aca04464155e2d78ff4ff56cadb
seonhoshin <shinwder@gmail.com>
2023-08-28 23:52:44 +0900

    [Init] springboot project 생성

---
+++ server/src/main/java/com/project/bbibbi/BbibbiApplication.java
@@ -0,0 +1,13 @@
+package com.project.bbibbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbibbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbibbiApplication.class, args);
+	}
+
+}
