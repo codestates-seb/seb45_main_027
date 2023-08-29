6243fbfa9245b689e9802895daa1af55d3370f71
yebin <yebind3308@gmail.com>
2023-08-29 18:22:05 +0900

    [Feat] #13 - 테일윈드 설치, props 설정

---
+++ client/src/components/common/Background.jsx
@@ -0,0 +1,12 @@
+export const backgrounMain = `flex justify-center w-full`;
+export const backgrounWrap = `flex w-full max-w-[1440px]`;
+
+const Background = () => {
+  return (
+    <main className={backgrounMain}>
+      <div className={backgrounWrap}></div>
+    </main>
+  );
+};
+
+export default Background;
--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import {backgrounMain} from '../components/common/Background'
+import { backgrounWrap } from "../components/common/Background";
 
 const Main = () => {
-  return <div>Main</div>;
+
+  return (
+    <main className={backgrounMain}>
+      <div className={backgrounWrap}>메인</div>
+    </main>
+  );
 };
 
 export default Main;
