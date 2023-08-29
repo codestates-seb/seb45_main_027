133e9d1f821f8767db1d19e041f03938aa1dc6d1
Yuri Choi <skdbflsk@gmail.com>
2023-08-29 22:04:26 +0900

    [Feat]#9 - signup,login,myinfo 헤더푸터  제외

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import "./index.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import HeaderPc from "./components/header/HeaderPc";
 import FooterPc from "./components/footer/FooterPc";
 import Login from "./pages/Login";
@@ -11,9 +11,13 @@ import Tips from "./pages/Tips";
 import Map from "./pages/Map";
 
 function App() {
+  const location = useLocation();
+  const shouldShowHeaderFooter = !['/signup', '/login'].includes(location.pathname);
+
+
   return (
     <>
-      <HeaderPc />
+      {shouldShowHeaderFooter && <HeaderPc />}
       <Routes>
         <Route path={"/"} element={<Main />} />
         <Route path={"/login"} element={<Login />} />
@@ -24,7 +28,7 @@ function App() {
         <Route path={"/map"} element={<Map />} />
       </Routes>
 
-      <FooterPc />
+      {shouldShowHeaderFooter && <FooterPc />}
     </>
   );
 }
