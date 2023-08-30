4b3d2defc262b1a45dc1fc2f8a8d52f63887122b
WONHO22 <dnjsgh2122@gmail.com>
2023-08-30 15:02:14 +0900

    [Feat] 카카오지도 API적용, 지도 내 marker 작업 완료

--- client/src/pages/Map.jsx
+++ client/src/pages/Map.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Background from "../components/common/Background";
 import KakaoMap from "../components/map/KakaoMap";
 
