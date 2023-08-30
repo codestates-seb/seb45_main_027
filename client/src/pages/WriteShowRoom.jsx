2c1c5670e8565a3c1c5735b7a891ef0624108a07
yebin <yebind3308@gmail.com>
2023-08-30 15:01:50 +0900

    [Feat] #14 - 헤더 (모바일 ver.)

---
+++ client/src/pages/WriteShowRoom.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderMobile from "../components/header/HeaderMobile";
+
+const WriteShowRoom = () => {
+    return (
+      <div>
+        <HeaderMobile buttonBgColor="bg-[#F5634A]" />
+      </div>
+    );
+};
+
+export default WriteShowRoom;
---
+++ client/src/pages/WriteTips.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderMobile from "../components/header/HeaderMobile";
+
+const WriteTips = () => {
+    return (
+      <div>
+        <HeaderMobile buttonBgColor="bg-[#00647B]" />
+      </div>
+    );
+};
+
+export default WriteTips;
