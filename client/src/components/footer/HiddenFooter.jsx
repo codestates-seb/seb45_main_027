1daed8a925e76520c0353ae5fd32e9a514a7aee5
yebin <yebind3308@gmail.com>
2023-08-30 15:48:07 +0900

    [Feat] #15 - 푸터 (모바일 작성창 ver.)

--- client/src/components/footer/HiddenFooter.jsx
+++ client/src/components/footer/HiddenFooter.jsx
@@ -1,16 +1,26 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import FooterPc from "./FooterPc";
 import FooterMobile from "./FooterMobile";
+import FooterMobileWrite from "./FooterMobileWrite";
 
-const HiddenFooter = () => (
-  <>
-    <div className="hidden md:block">
-      <FooterPc />
-    </div>
-    <div className="md:hidden">
-      <FooterMobile />
-    </div>
-  </>
-);
+const HiddenFooter = () => {
+    const location = useLocation();
+    const path = location.pathname;
+
+    const isWrite =
+      path.includes("/tips/write") || path.includes("/showroom/write");
+
+    return (
+      <>
+        <div className="hidden md:block">
+          <FooterPc />
+        </div>
+        <div className="md:hidden">
+          {isWrite ? <FooterMobileWrite /> : <FooterMobile />}
+        </div>
+      </>
+    );
+}
 
 export default HiddenFooter;
