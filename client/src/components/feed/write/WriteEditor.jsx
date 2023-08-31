a780cf819cc057e7af831451de81e5434dfd4975
yebin <yebind3308@gmail.com>
2023-09-01 01:53:47 +0900

    [Feat] #17 - 작성창 (작성폼 이미지 입력 기능 추가)

--- client/src/components/feed/write/WriteEditor.jsx
+++ client/src/components/feed/write/WriteEditor.jsx
@@ -1,19 +1,33 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useRef } from "react";
+import WriteTag from "./WriteTag";
 
 const WriteEditor = () => {
-  const [editor, setEditor] = useState("");
   const [title, setTitle] = useState("");
-  const [tags, setTags] = useState([]);
-  const tagInputRef = useRef(null);
+  const [editor, setEditor] = useState("내용을 입력해주세요");
+  const [isFocused, setIsFocused] = useState(false);
+  const editorRef = useRef(null);
+  
+  const handleFocus = () => {
+    if (!isFocused) {
+      setEditor("");
+    }
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    if (editor === "") {
+      setEditor("내용을 입력해주세요");
+    }
+    setIsFocused(false);
+  };
+
+  const handleChange = (e) => {
+    setEditor(e.target.innerHTML);
+  };
 
   // 타이틀 변경
   const handleTitleChange = (e) => {
     setTitle(e.target.value);
-  };
-
-  // 텍스트 에어리어 변경
-  const handleTextChange = (e) => {
-    setEditor(e.target.value);
   };
 
   // 이미지 업로드
@@ -24,33 +38,22 @@ const WriteEditor = () => {
       reader.readAsDataURL(file);
       reader.onloadend = () => {
         const imageUrl = reader.result;
-        setEditor(`${editor}\n![Uploaded Image](${imageUrl})`);
+        const imgTag = document.createElement("img");
+        imgTag.src = imageUrl;
+        imgTag.alt = "Uploaded Image";
+
+        const brTag = document.createElement("br"); // 새 줄을 위한 <br> 태그 생성
+
+        if (editorRef.current) {
+          editorRef.current.appendChild(imgTag);
+          editorRef.current.appendChild(brTag); // <img> 태그 다음에 <br> 태그 추가
+          editorRef.current.focus(); // 이미지와 <br> 태그 삽입 후, 다시 div에 포커스 주기
+        }
+
+        e.target.value = null; // input의 value를 초기화해서 중복 업로드를 가능하게 함
       };
     }
   };
-
-  // 태그 추가
-  const handleTagKeyDown  = (e) => {
-    if (e.key === "Enter") {
-      e.preventDefault();
-      if (e.target.value.trim() !== "") {
-        setTags([...tags, `#${e.target.value.trim()}`]);
-        e.target.value = "";
-      }
-    }
-  };
-
-  // 태그 삭제
-  const handleTagClick = (index) => {
-    const newTags = [...tags];
-    newTags.splice(index, 1);
-    setTags(newTags);
-  };
-
-  // 태그 인풋에 포커스
-  useEffect(() => {
-    tagInputRef.current.focus();
-  }, [tags]);
 
   return (
     <div className="mt-10 mb-20 p-4 bg-white w-full h-full">
@@ -67,7 +70,7 @@ const WriteEditor = () => {
       </div>
 
       {/* 이미지추가 버튼 */}
-      <div className="mb-2 w-full">
+      <div className="mb-2 pb-2 border-b">
         <label htmlFor="imageUpload" className="cursor-pointer">
           <img className="p-2" src="/images/gallery.png" alt="" />
         </label>
@@ -82,35 +85,16 @@ const WriteEditor = () => {
       </div>
 
       {/* 입력창 */}
-      <textarea
-        value={editor}
-        onChange={handleTextChange}
-        rows="30"
-        placeholder="내용을 입력해주세요."
-        className="w-full p-2 border-b"
+      <div
+        ref={editorRef}
+        contentEditable={true}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        onInput={handleChange}
+        dangerouslySetInnerHTML={{ __html: editor }}
+        className="w-full h-full min-h-[500px] p-2 border-b "
       />
-
-      {/* 태그 */}
-      <div className="mt-4">
-        <div className="flex flex-wrap items-center ">
-          {tags.map((tag, index) => (
-            <span
-              key={index}
-              className="m-1 p-1 border rounded-md cursor-pointer"
-              onClick={() => handleTagClick(index)}>
-              {tag}
-            </span>
-          ))}
-          <input
-            id="tags"
-            type="text"
-            ref={tagInputRef}
-            onKeyDown={handleTagKeyDown}
-            placeholder="#태그"
-            className="mt-1 p-1 border rounded-md"
-          />
-        </div>
-      </div>
+      <WriteTag />
     </div>
   );
 };
