c64cca6a224972db9e6b1c408cd68622ac1e0278
Ernest45 <readonme@naver.com>
2023-09-05 23:36:30 +0900

    [Fix] #80 - H2 authorizeRequests 설정 오류

--- server/src/main/java/com/project/bbibbi/auth/config/SecurityConfig.java
+++ server/src/main/java/com/project/bbibbi/auth/config/SecurityConfig.java
@@ -58,8 +58,8 @@ public class SecurityConfig {
 
                 // 아이콘, css, js 관련
                 // 기본 페이지, css, image, js 하위 폴더에 있는 자료들은 모두 접근 가능, h2-console에 접근 가능
-                .antMatchers("/","/css/**","/images/**","/js/**","/favicon.ico","/h2-console/**").permitAll()
-                .antMatchers("/signup").permitAll() // 회원가입 접근 가능
+                .antMatchers("/","/css/**","/images/**","/js/**","/favicon.ico","/h2/**").permitAll()
+                .antMatchers("/auth/**").permitAll() // 회원가입 접근 가능
                 .anyRequest().authenticated() // 위의 경로 이외에는 모두 인증된 사용자만 접근 가능
                 .and()
                 //== 소셜 로그인 설정 ==//
