33c5c5342af5c7799e46e916be42bf833bd3531f
yebin <yebind3308@gmail.com>
2023-09-07 16:48:51 +0900

    [Design] - CSS 통합

--- client/src/components/showroom/all/AllContent.jsx
+++ client/src/components/showroom/all/AllContent.jsx
@@ -27,7 +27,7 @@ const data = [
   { url: "./asset/image4.png", isBookmarked: false },
 ];
 
-const AllContent = ({ viewportWidth, setViewportWidth }) => {
+const AllContent = () => {
   const [image, setImage2] = useState(data); // 이미지데이터를 상태로 저장
 
   // 북마크 상태를 변경시켜주는 함수
@@ -37,56 +37,53 @@ const AllContent = ({ viewportWidth, setViewportWidth }) => {
     setImage2(updatedBookmarks);
     console.log(updatedBookmarks);
   };
-  // width가 모바일인지 판별
-  const isMobileView = viewportWidth < 720;
-
-  //1350 이하
+  
   return (
-    <div className="flex-col ">
-      <div className="flex pt-5 justify-between flex-wrap">
+    <div className="flex-col mx-4">
+      <div className="flex justify-between flex-wrap">
         {image.map((item, idx) => (
           <div
             key={idx}
-            className={`flex-col relative mx-3 mb-3 ${
-              isMobileView ? "w-[45%]" : "w-[30%]"
-            } h-[20%]`}
-          >
-            <img
-              src={item.url}
-              alt="shroomimg"
-              className="w-full h-[330px] rounded-xl "
-            />
-            <p>
+            className="flex-col mx-3 mb-3 w-full sm:w-[45%] lg:w-[30%] h-[20%]">
+            <div className="relative">
               <img
-                src={
-                  item.isBookmarked
-                    ? "./images/isBookmarked.png"
-                    : "./images/Bookmark.png"
-                }
-                alt="Bookmark"
-                className="absolute bottom-44 right-4 cursor-pointer"
-                style={{
-                  width: "30px",
-                  height: "30px",
-                }}
-                onClick={() => toggleBookmark(idx)}
+                src={item.url}
+                alt="shroomimg"
+                className="aspectRatioImage_4_3 rounded-md"
               />
-            </p>
-            <div className="flex-col pt-2 mb-8">
-              <div className="flex justify-center mb-3">
-                <span className="text-3xl font-bold">Title.</span>
+              <p>
+                <img
+                  src={
+                    item.isBookmarked
+                      ? "./images/isBookmarked.png"
+                      : "./images/Bookmark.png"
+                  }
+                  alt="Bookmark"
+                  className="absolute w-10 h-10 bottom-4 right-4 cursor-pointer"
+                  onClick={() => toggleBookmark(idx)}
+                />
+              </p>
+            </div>
+            <div className="flex-col pt-2 mb-14">
+              <div className="flex justify-center my-3">
+                <span className="text-3xl font-semibold">Title.</span>
               </div>
-              <div className="flex justify-center mb-3">
+              <div className="flex justify-center items-center mb-3 text-gray-800">
                 <img
                   src="./images/Wonho.png"
                   alt="프로필사진"
-                  className="w-12 rounded-full mr-2"
-                ></img>
-                <span className="text-xl pt-[5px]">user name</span>
+                  className="w-6 h-6 rounded-full mr-2"></img>
+                <span className="text-xl">user name</span>
               </div>
-              <div className="flex justify-center">
-                <span className="pr-10 text-xl">스크랩: 0</span>
-                <span className="text-xl"> 조회수: 1</span>
+              <div className="flex justify-center text-lg text-gray-500">
+                <div className="mr-10">
+                  <span>스크랩 :</span>
+                  <span className="ml-1">0</span>
+                </div>
+                <div>
+                  <span>조회수 :</span>
+                  <span className="ml-1">0</span>
+                </div>
               </div>
             </div>
           </div>
--- client/src/components/showroom/all/AllHeader.jsx
+++ client/src/components/showroom/all/AllHeader.jsx
@@ -13,25 +13,27 @@ const AllHeader = ({ viewportWidth, setViewportWidth }) => {
 
   const isMobileView = viewportWidth < 720; // viewportWidth가 720보다 작으면 모바일 화면으로 간주
   return (
-    <div className="flex justify-between flex-wrap">
-      <div className="flex items-center">
-        <h1 className="pt-1 text-4xl text-[#F5634A] font-semibold">All</h1>
-        <div className="pl-4 pt-6 text-xl  flex-nowrap">
-          <span className="pr-3 text-[16px]">최신순</span>
-          <span className="pr-3 text-[16px]">인기순</span>
-          <span className="pr-3 text-[16px]">공간별</span>
-          <span className="pr-3 text-[16px]">평수별</span>
-          <span className="pr-3 text-[16px]">주거형태별</span>
+    <div className="flex justify-between items-center flex-wrap mx-8">
+      <div className="flex items-baseline">
+        <h1 className="text-4xl text-[#F5634A] font-semibold Showcard-Gothic">
+          All
+        </h1>
+        <div className="text-xl flex-nowrap pl-4">
+          <span className="pr-3">최신순</span>
+          <span className="pr-3">인기순</span>
+          <span className="pr-3">공간별</span>
+          <span className="pr-3">평수별</span>
+          <span className="pr-3">주거형태별</span>
         </div>
       </div>
-      <div className={`relative ${isMobileView ? "mx-auto pr-5" : ""}`}>
+      <div className={`relative flex items-center my-5 ${isMobileView ? "mx-auto pr-5" : ""}`}>
         <img
-          className="absolute w-[28px] top-[13px] left-4"
+          className="absolute w-6 left-4"
           src="./images/Search_gray.png"
           alt="searchIamge"
         />
         <input
-          className="w-[330px] mt-3 mr-3 py-3 pl-14 pr-4 rounded-3xl border border-[#BBBBBB]"
+          className="w-[330px] py-3 pl-12 pr-4 rounded-md border"
           type="text"
           value={inputValue}
           onChange={handleInputChange}
