f3deeaae886ca5eb6eb90c7e3656175407052499
WONHO22 <dnjsgh2122@gmail.com>
2023-09-11 16:55:01 +0900

    [Feat] #17, #63 - tip페이지 서버통신 로직 구현완료

--- client/src/pages/Tips.jsx
+++ client/src/pages/Tips.jsx
@@ -3,9 +3,32 @@ import Background from "../components/common/Background";
 import TipsHeader from "../components/tips/TipsHeader";
 import TipsContent from "../components/tips/TipsContent";
 import FooterMobile from "../components/footer/FooterMobile";
+import useAxios from "../hooks/useAxios";
+import { toast } from "react-hot-toast";
 
 const Tips = () => {
   const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+  const [tipData, setTipData] = useState("");
+
+  const configParams = {
+    method: "GET",
+    url: "/feed/filter/RECENT00",
+    headers: {
+      "Content-Type": "application/json",
+      "ngrok-skip-browser-warning": "69420",
+    },
+  };
+
+  const [response, error, loading] = useAxios(configParams);
+
+  useEffect(() => {
+    if (response) {
+      setTipData(response.data.data);
+    } else if (error) {
+      console.error("Error:", error);
+    }
+  }, [response, error]);
+  console.log(tipData);
 
   useEffect(() => {
     // 반응형 조건부렌더링
@@ -20,12 +43,32 @@ const Tips = () => {
     };
   }, []);
 
+  // 로딩시 토스트 및 이미지 처리
+  useEffect(() => {
+    if (!tipData && loading) {
+      toast.loading("로딩중...");
+    } else if (tipData || error) {
+      toast.dismiss();
+    }
+  }, [tipData, loading, error]);
+
+  if (!tipData) {
+    return (
+      <div className="flex justify-center h-auto">
+        <img
+          src="https://homepagepictures.s3.ap-northeast-2.amazonaws.com/client/public/images/loading.gif"
+          alt="로딩중"
+        />
+      </div>
+    );
+  }
+
   return (
     <>
       <Background mainclassName="h-full bg-[#FFFAEE]" divclassName="">
         <div className="flex-col w-full">
-          <TipsHeader viewportWidth={viewportWidth}></TipsHeader>
-          <TipsContent viewportWidth={viewportWidth}></TipsContent>
+          <TipsHeader viewportWidth={viewportWidth} />
+          <TipsContent tipData={tipData} />
         </div>
       </Background>
       {viewportWidth < 720 && <FooterMobile />}
