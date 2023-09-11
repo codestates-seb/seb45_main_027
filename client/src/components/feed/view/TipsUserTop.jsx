6a35c7a02d9687653aa22863bfec97566d729315
yebin <yebind3308@gmail.com>
2023-09-11 16:54:12 +0900

    [Feat] #108 - 유저정보, 본문글 통신 구현

--- client/src/components/feed/view/TipsUserTop.jsx
+++ client/src/components/feed/view/TipsUserTop.jsx
@@ -1,22 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { toast } from "react-hot-toast";
+import useAxios from "../../../hooks/useAxios";
+
 
 const buttonStyle =
   "flex items-center justify-center rounded-lg shadow w-32 h-full";
 
 const TipsUserTop = () => {
   const [follow, setFollow] = useState(false);
+  const [nickname, setNickname] = useState(null);
+  const [createdDate, setCreatedDate] = useState(null);
+  const { tipId } = useParams();
+  console.log(nickname);
+
+  const configParams = {
+    method: "GET",
+    url: `/tip/${tipId}`,
+    headers: {
+      "ngrok-skip-browser-warning": "69420",
+    },
+  };
+
+  const [response, error, loading] = useAxios(configParams);
+
+  useEffect(() => {
+    if (response) {
+      setNickname(response.data.nickname);
+      const dateTime = response.data.createdDateTime.split("T")[0]; // T 뒤에 시간 부분은 받아오지 않기 위해서 추가
+      setCreatedDate(dateTime);
+    } else if (error) {
+      console.error("Error:", error);
+    }
+  }, [response, error]);
+
+  useEffect(() => {
+    if (!nickname && !createdDate && loading) {
+      toast.loading("로딩중...");
+    } else if ((nickname && createdDate) || error) {
+      toast.dismiss();
+    }
+  }, [nickname, createdDate, loading, error]);
 
   return (
     <div className="flex justify-between pt-20">
       <div className="flex items-center">
-        <div className="border w-12 h-12 rounded-full bg-red-500 mr-4" />
+        <div className="border w-12 h-12 rounded-full bg-red-500 mr-4" ></div>
         <div>
-          <div className="text-lg font-semibold">유저이름</div>
-          <div className="text-gray-500">게시 날짜</div>
+          <div className="text-lg font-semibold">{nickname}</div>
+          <div className="text-gray-500">{createdDate}</div>
         </div>
       </div>
-      <button
-        onClick={() => setFollow((prevFollow) => !prevFollow)}>
+      <button onClick={() => setFollow((prevFollow) => !prevFollow)}>
         {follow ? (
           <div className={`bg-white ${buttonStyle} `}>
             <img
--- client/src/components/feed/view/ViewForm.jsx
+++ client/src/components/feed/view/ViewForm.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { toast } from "react-hot-toast";
+import useAxios from "../../../hooks/useAxios";
 
 const ViewForm = () => {
-    return (
-      <div className="w-full h-[800px] border-8 mt-20">
-        <span className=" text-5xl">본문 내용</span>
-      </div>
-    );
+  const { tipId } = useParams();
+  const [content, setContent] = useState(null);
+  console.log(content);
+
+  const configParams = {
+    method: "GET",
+    url: `/tip/${tipId}`,
+    headers: {
+      "ngrok-skip-browser-warning": "69420",
+    },
+  };
+
+  const [response, error, loading] = useAxios(configParams);
+
+  useEffect(() => {
+    if (response) {
+      setContent(response.data.content);
+    } else if (error) {
+      console.error("Error:", error);
+    }
+  }, [response, error]);
+
+  useEffect(() => {
+    if (!content && loading) {
+      toast.loading("로딩중...");
+    } else if (content || error) {
+      toast.dismiss();
+    }
+  }, [content, loading, error]);
+
+  return (
+    <div className="w-full h-full py-10 border mt-20">
+      <span className=" text-5xl">{content}</span>
+    </div>
+  );
 };
 
-export default ViewForm; 
+export default ViewForm;
