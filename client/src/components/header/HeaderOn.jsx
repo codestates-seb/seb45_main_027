93f140ff0e88b81cba5216827773bea31c500e40
Yuri Choi <skdbflsk@gmail.com>
2023-09-12 04:31:32 +0900

    [Feat] #9 로그인시 토큰 관리 로직 추가

--- client/src/components/header/HeaderOn.jsx
+++ client/src/components/header/HeaderOn.jsx
@@ -1,14 +1,14 @@
 // 로그인 후 헤더 값
-
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../../context/AuthContext"
-
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
 
 const HeaderOn = () => {
   const { logout } = useAuth();
   const navigate = useNavigate();
   const memberId = localStorage.getItem("memberId");
+  const nickname = localStorage.getItem("nickname");
+  const profileImg = localStorage.getItem("profileImg");
 
   const handleLogout = async () => {
     try {
@@ -20,31 +20,31 @@ const HeaderOn = () => {
     navigate(`/myinfo/${memberId}`);
   };
 
-    return (
-      <div className="flex">
-        <ul className="flex text-[#F5634A] text-xl font-bold">
-          {/* 유저정보 */}
-          {/* <Link to="/myinfo"> */}
-            <li className="flex items-center" onClick={handleUserClick}>
-              <div className="w-8 h-8 mr-2.5 bg-yellow-200" />
-              <span className="pr-8 text-gray-700">닉네임</span>
-            </li>
-          {/* </Link> */}
+  return (
+    <div className="flex">
+      <ul className="flex text-[#F5634A] text-xl font-bold">
+        {/* 유저정보 */}
+        <li className="flex items-center" onClick={handleUserClick}>
+          {profileImg ? (
+            <img src={profileImg} alt="profileImg" className='"w-8 h-8 mr-2.5 rounded-full' />
+          ) : (
+            <div className="w-8 h-8 mr-2.5 bg-yellow-200 rounded-full" />
+          )}
+          <span className="pr-8 text-gray-700">{nickname}</span>
+        </li>
 
-          {/* 로그아웃  */}
-          {/* <Link to="/signup"> */}
-            <li className="flex items-center" onClick={handleLogout}>
-              <img
-                className="flex pr-2.5 h-5"
-                src="/images/logout.png"
-                alt=""
-              />
-              <span className="Showcard-Gothic">Logout</span>
-            </li>
-          {/* </Link> */}
-        </ul>
-      </div>
-    );
+        {/* 로그아웃  */}
+        <li className="flex items-center" onClick={handleLogout}>
+          <img
+            className="flex pr-2.5 h-5"
+            src="https://homepagepictures.s3.ap-northeast-2.amazonaws.com/client/public/images/logout.png"
+            alt=""
+          />
+          <span className="Showcard-Gothic">Logout</span>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default HeaderOn;
--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -9,68 +9,13 @@ export function useAuth() {
 }
 
 export function AuthProvider({ children }) {
-  const [user, setUser] = useState(null); 
+  const [user, setUser] = useState(null);
   const navigate = useNavigate();
 
   // const baseURL =
   //   "http://ec2-3-39-231-102.ap-northeast-2.compute.amazonaws.com:8080";
 
   const baseURL = process.env.REACT_APP_API_URL;
-
-
-  // useEffect(() => {
-  //   // 엑세스토큰 체크하고 리프레시토큰 받아오는 부분
-  //   async function checkAuthStatus() {
-  //     const storedAccessToken = localStorage.getItem("accessToken");
-  //     const storedRefreshToken = localStorage.getItem("refreshToken");
-
-  //     // 로컬스토리지안에 저장되어 있는 엑세스 토큰이 있을때만 아래코드 실행
-  //     if (storedAccessToken) {
-  //       axios.defaults.headers.common[
-  //         "Authorization"
-  //       ] = `Bearer ${storedAccessToken}`;
-  //      // 엑세스토큰을 매 요청시 헤더에 싣는부분
-
-  //       try {
-  //         // 가지고 있는 엑세스토큰이 유효한지 확인
-  //         await axios.get(`${baseURL}/auth/check-auth`);
-  //       } catch (error) {
-  //         if (
-  //           error.response &&
-  //           error.response.status === 400 &&
-  //           storedRefreshToken
-  //         ) {
-  //           // 엑세스토큰이 만료되었고 리프레시토큰만 있을때, 가지고 있는 리프레시 토큰으로 다시 엑세스토큰 받아옴
-  //           try {
-  //             const refreshResponse = await axios.post(
-  //               `${baseURL}/auth/refresh-token`,
-  //               {
-  //                 refreshToken: storedRefreshToken,
-  //               }
-  //             );
-
-  //             // 새로 받은 엑세스토큰을 Axios 헤더에 기본값으로 넣고, 로컬스토리지에도 저장함
-  //             const newAccessToken = refreshResponse.data.accessToken;
-  //             localStorage.setItem("accessToken", newAccessToken);
-  //             axios.defaults.headers.common[
-  //               "Authorization"
-  //             ] = `Bearer ${newAccessToken}`;
-  //           } catch (refreshError) {
-  //             // 리프레시 토큰 사용 실패했을때
-  //             console.error("Token refresh failed:", refreshError);
-  //             //navigate("/login");
-  //           }
-  //         } else {
-  //           // 또 다른 오류들이나 리프레시토큰이 만료됐을때
-  //           console.error("Authentication error:", error);
-  //           //navigate("/login");
-  //         }
-  //       }
-
-  //   }
-
-  //   checkAuthStatus();
-  // }, [navigate]);
 
   useEffect(() => {
     const storedAccessToken = localStorage.getItem("accessToken");
@@ -92,17 +37,9 @@ export function AuthProvider({ children }) {
         password,
       });
       console.log(response);
-
-      const match = response.data.match(/{member-id} : (\d+)/);
-      if (match) {
-        const memberIdValue = match[1];
-        console.log(memberIdValue);
-        setUser(memberIdValue);
-        localStorage.setItem("memberId", memberIdValue);
-      } else {
-        console.error("Value not found");
-      }
-
+      localStorage.setItem("memberId", response.data.memberId);
+      localStorage.setItem("nickname", response.data.nickname);
+      localStorage.setItem("profileImg", response.data.profileImg);
       localStorage.setItem("accessToken", response.headers["authorization"]);
       localStorage.setItem(
         "refreshToken",
@@ -111,15 +48,12 @@ export function AuthProvider({ children }) {
       navigate("/");
     } catch (error) {
       //불일치시 401에러
-      //인증완료되지 않은 이메일 처리도 해야됨 403주면 인증페이지로 보내기
-      // if (error.response.status === 403) {
-      //   alert("이메일 인증을 완료해주세요.");
-      //   //navigate("/verify"); 
-      // } else {
-        console.error("Login failed:", error);
-        alert("Login failed");
-        throw error;
-      //}
+      if(error.response.status === 401){
+        alert("이메일 또는 비밀번호가 일치하지 않습니다.");
+      };
+      console.error("Login failed:", error);
+      alert("Login failed");
+      throw error;
     }
   }
 
@@ -128,20 +62,11 @@ export function AuthProvider({ children }) {
       const response = await axios.get(
         `${baseURL}/auth/oauth/kakao?code=${code}`
       );
-
-      //어디서 보낸건지 서버에서 알고 싶다면 이런 방법도 있다
-      // const requestData = {
-      //   code,
-      //   provider: "kakao",
-      // };
-      // const response = await axios.post(`${baseURL}/auth/oauth`, requestData);
-
       localStorage.setItem("accessToken", response.data.accessToken);
       localStorage.setItem("refreshToken", response.data.refreshToken);
       navigate("/");
     } catch (error) {
       console.log(code);
-
       throw error;
     }
   }
@@ -184,6 +109,8 @@ export function AuthProvider({ children }) {
       localStorage.removeItem("accessToken");
       localStorage.removeItem("refreshToken");
       localStorage.removeItem("memberId");
+      localStorage.removeItem("nickname");
+      localStorage.removeItem("profileImg");
       setUser(null);
 
       navigate("/login");
