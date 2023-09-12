326be0ad54abb289159274b986f08c1eb2d6e6dc
yebin <yebind3308@gmail.com>
2023-09-12 16:53:46 +0900

    [Feat] #108 - 댓글 답글 입력기능 구현

--- client/src/components/feed/view/Comment.jsx
+++ client/src/components/feed/view/Comment.jsx
@@ -1,125 +1,166 @@
 import React, { useState, forwardRef  } from "react";
 
 //  함수형 컴포넌트는 ref 속성을 가질 수 없지만, forwardRef를 사용하면 이러한 제약을 우회할 수 있다.
-const Comment = forwardRef((props, ref) => {  // 사이드바에서 댓글 클릭시 이동을 위해 받아 옴.
+const Comment = forwardRef((props, ref) => { // 사이드바에서 댓글 클릭시 이동을 위해 받아 옴.
   // 댓글 좋아요
-  const [like, setLike] = useState(false);
+  const [like, setLike] = useState({});
   // 답글 달기
-  const [showReply, setShowReply] = useState(false);
+  const [showReply, setShowReply] = useState({});
+  // 댓글/답글 입력값을 저장할 State
+  const [commentInput, setCommentInput] = useState("");
+  const [replyInput, setReplyInput] = useState("");
+  // 작성된 댓글/답글을 저장할 State
+  const [comments, setComments] = useState([]);
+  const [replies, setReplies] = useState({});
 
-    return (
-      <div className="mt-10" ref={ref}>
-        {/* 댓글 수 표시 */}
-        <div className="flex">
-          <span className="text-xl font-semibold">댓글</span>
-          <span className="text-xl font-semibold text-[#35C5F0] ml-2">0</span>
-        </div>
+  const handleCommentSubmit = () => {
+    setComments((prevComments) => [...prevComments, commentInput]);
+    setReplies((prevReplies) => ({ ...prevReplies, [commentInput]: [] }));
+    setCommentInput("");
+  };
 
-        {/* 댓글 입력창 */}
-        <div className="flex w-full mt-4">
-          <img
-            src="https://homepagepictures.s3.ap-northeast-2.amazonaws.com/client/public/images/userComment.png"
-            alt="유저사진"
+  // 좋아요 각 상태관리
+  const toggleLike = (comment) => {
+    setLike((prevLikes) => ({
+      ...prevLikes,
+      [comment]: !prevLikes[comment],
+    }));
+  };
+
+  // 답글달기 각 상태관리
+  const toggleReply = (comment) => {
+    setShowReply((prevShowReply) => ({
+      ...prevShowReply,
+      [comment]: !prevShowReply[comment],
+    }));
+  };
+
+  
+  const handleReplySubmit = (comment) => {
+    setReplies((prevReplies) => ({
+      ...prevReplies,
+      [comment]: [...prevReplies[comment], replyInput],
+    }));
+    setReplyInput("");
+  };
+
+  return (
+    <div className="mt-10" ref={ref}>
+      {/* 댓글 수 표시 */}
+      <div className="flex">
+        <span className="text-xl font-semibold">댓글</span>
+        <span className="text-xl font-semibold text-[#35C5F0] ml-2">0</span>
+      </div>
+
+      {/* 댓글 입력창 */}
+      <div className="flex w-full mt-4">
+        <img
+          src="https://homepagepictures.s3.ap-northeast-2.amazonaws.com/client/public/images/userComment.png"
+          alt="유저사진"
+        />
+        <div className="flex w-full relative">
+          <input
+            className="h-full w-full ml-4 border rounded-lg pl-4"
+            value={commentInput}
+            onChange={(e) => setCommentInput(e.target.value)}
           />
-          <div className="flex w-full relative">
-            <input className="h-full w-full ml-4 border rounded-lg pl-4" />
-            <button className=" absolute right-0 top-1/4 pr-4"> 입력 </button>
-          </div>
+          <button
+            className="absolute right-0 top-1/4 pr-4"
+            onClick={handleCommentSubmit}>
+            입력
+          </button>
         </div>
+      </div>
 
-        <div className="flex flex-col">
-          {/* 댓글 작성보기 */}
-          <div className="flex flex-col items-start mt-10">
-            <div className="flex items-start">
+      {/* 댓글 출력창 */}
+      {comments.map((comment, index) => (
+        <div key={index} className="flex flex-col mt-10">
+          <div className="flex items-start">
+            <img
+              src="https://homepagepictures.s3.ap-northeast-2.amazonaws.com/client/public/images/userComment.png"
+              alt="유저사진"
+            />
+            <div className="flex flex-col ml-4 w-full">
+              <span className="text-lg font-semibold">댓글 작성자</span>
+              <span className="my-4 text-base">{comment}</span>
+              {/* 작성날짜, 좋아요, 답글 */}
+              <div className="flex items-center text-gray-500 font-medium text-base">
+                {/* 작성날짜 */}
+                <span>작성날짜</span>
+
+                {/* 좋아요 */}
+                <button
+                  className="flex items-center mx-4"
+                  onClick={() => toggleLike(comment)}>
+                  <img
+                    className="w-6 h-6 mr-1"
+                    src={
+                      like[comment]
+                        ? "https://homepagepictures.s3.ap-northeast-2.amazonaws.com/client/public/images/heart-on.png"
+                        : "https://homepagepictures.s3.ap-northeast-2.amazonaws.com/client/public/images/heart-off.png"
+                    }
+                    alt="좋아요"
+                  />
+                  <span>좋아요</span>
+                </button>
+
+                {/* 답글 */}
+                <button onClick={() => toggleReply(comment)}>답글 달기</button>
+              </div>
+            </div>
+          </div>
+
+          {/* 답글 입력창 */}
+          {showReply[comment] && (
+            <div className="flex w-full mt-6 ml-10">
               <img
                 src="https://homepagepictures.s3.ap-northeast-2.amazonaws.com/client/public/images/userComment.png"
                 alt="유저사진"
               />
+              <div className="flex w-full relative">
+                <input
+                  className="h-full w-full ml-4 border rounded-lg pl-4"
+                  value={replyInput}
+                  onChange={(e) => setReplyInput(e.target.value)}
+                />
+                <button
+                  className="absolute right-0 top-1/4 pr-4"
+                  onClick={() => handleReplySubmit(comment)}>
+                  입력
+                </button>
+              </div>
+            </div>
+          )}
 
-              <div className="flex flex-col ml-4 w-full">
-                {/* 작성자 */}
-                <span className="text-lg font-semibold">질문 작성자</span>
+          {/* 답글 출력창 */}
+          {replies[comment] &&
+            replies[comment].map((reply, replyIndex) => (
+              <div className="flex items-start mt-10 ml-10 bg-[#fceecd] p-8 rounded-lg shadow">
+                <img
+                  src="https://homepagepictures.s3.ap-northeast-2.amazonaws.com/client/public/images/userComment.png"
+                  alt="유저사진"
+                />
+                <div className="flex flex-col ml-4 w-full">
+                  {/* 답변 작성자 */}
+                  <span className="text-lg font-semibold">답변 작성자</span>
 
-                {/* 댓글 내용 */}
-                <span className="my-4 text-base">
-                  댓글내용 댓글내용 댓글내용 댓글내용 댓글내용 댓글내용 댓글내용
-                  댓글내용 댓글내용 댓글내용 댓글내용 댓글내용 댓글내용 댓글내용
-                </span>
-
-                {/* 작성날짜, 좋아요, 답글 */}
-                <div className="flex items-center text-gray-500 font-medium text-base">
-                  {/* 작성날짜 */}
-                  <span>작성날짜</span>
-
-                  {/* 좋아요 */}
-                  <button
-                    className="flex items-center mx-4"
-                    onClick={() => setLike(!like)}>
-                    <img
-                      className="w-6 h-6 mr-1"
-                      src={
-                        like
-                          ? "https://homepagepictures.s3.ap-northeast-2.amazonaws.com/client/public/images/heart-on.png"
-                          : "https://homepagepictures.s3.ap-northeast-2.amazonaws.com/client/public/images/heart-off.png"
-                      }
-                      alt="좋아요"
-                    />
-                    <span>좋아요</span>
-                  </button>
-
-                  {/* 답글 */}
-                  <button onClick={() => setShowReply(!showReply)}>
-                    답글 달기
-                  </button>
-                </div>
-
-                {/* 답글 입력창 */}
-                {showReply && (
-                  <div className=" flex w-full mt-6">
-                    <img
-                      src="https://homepagepictures.s3.ap-northeast-2.amazonaws.com/client/public/images/userComment.png"
-                      alt="유저사진"
-                    />
-                    <div className="flex w-full relative">
-                      <input className="h-full w-full ml-4 border rounded-lg pl-4" />
-                      <button className=" absolute right-0 top-1/4 pr-4">
-                        입력
-                      </button>
-                    </div>
+                  {/* 댓글 내용 */}
+                  <div key={replyIndex} className="my-4 text-base">
+                    {reply}
                   </div>
-                )}
 
-                {/* 답글 작성보기 */}
-                <div className="flex items-start mt-10 bg-[#fceecd] p-8 rounded-lg shadow">
-                  <img
-                    src="https://homepagepictures.s3.ap-northeast-2.amazonaws.com/client/public/images/userComment.png"
-                    alt="유저사진"
-                  />
-
-                  <div className="flex flex-col ml-4 w-full">
-                    {/* 작성자 */}
-                    <span className="text-lg font-semibold">답변 작성자</span>
-
-                    {/* 댓글 내용 */}
-                    <span className="my-4 text-base">
-                      답글내용 답글내용 답글내용 답글내용 답글내용 답글내용
-                      답글내용 답글내용 답글내용 답글내용 답글내용 답글내용
-                      답글내용 답글내용
-                    </span>
-
+                  {/* 작성날짜 */}
+                  <div className="flex items-center text-gray-500 font-medium text-base">
                     {/* 작성날짜 */}
-                    <div className="flex items-center text-gray-500 font-medium text-base">
-                      {/* 작성날짜 */}
-                      <span>작성날짜</span>
-                    </div>
+                    <span>작성날짜</span>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
+            ))}
         </div>
-      </div>
-    );
+      ))}
+    </div>
+  );
 });
 
 export default Comment;
