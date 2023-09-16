0b63bbfe8412c96fc63d5af566d4ac25f3fae91e
WONHO22 <dnjsgh2122@gmail.com>
2023-09-16 18:27:10 +0900

    [Feat] #108 - 마크다운 content데이터 화면에 렌더링 로직 구현

--- client/src/components/feed/view/ViewForm.jsx
+++ client/src/components/feed/view/ViewForm.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 const ViewForm = ({ feedData }) => {
+  console.log(feedData.content);
   return (
     <div className="w-full h-full py-10 border mt-20">
-      <span className=" text-5xl">{feedData.content}</span>
+      <div
+        className="text-5xl"
+        dangerouslySetInnerHTML={{ __html: feedData.content }}
+      />
     </div>
   );
 };
