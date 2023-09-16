40ce3630802fd5c596bc97522fd8e75f3dbc7b0b
yebin <yebind3308@gmail.com>
2023-09-16 19:21:59 +0900

    [Fix] #176 - 댓글창 POST 기능 구현

--- client/src/components/feed/comment/CommentInput.jsx
+++ client/src/components/feed/comment/CommentInput.jsx
@@ -1,9 +1,64 @@
-import React, { useState,  useEffect } from "react";
-import useAxios from "../../../hooks/useAxios";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { toast } from "react-hot-toast";
+import api from "../../common/tokens";
 
-const CommentInput = ({ feedData }) => {
+const CommentInput = ({ feedData, setFeedData }) => {
+  const profileImg = localStorage.getItem("profileImg");
+  const { feedId } = useParams(); // 게시물 번호
+
+  // 입력할 댓글
+  const [inputComment, setInputComment] = useState("");
+  // 입력한 댓글
+  const [enterComment, setEnterComment] = useState([]);
+
+  //POST 요청
+  const postComment = async () => {
+    const configParams = {
+      method: "POST",
+      url: `/feed/${feedId}/feedReply`,
+      headers: {
+        "ngrok-skip-browser-warning": "69420",
+      },
+      data: {
+        content: inputComment,
+      },
+    };
+    try {
+      const res = await api(configParams);
+      if (res && res.status === 200) {
+        const newComment = res.data;
+        setEnterComment((prevComments) => [...prevComments, newComment]);
+
+        const addCommentCout = feedData.repliesCount + 1; // 기존 댓글 수 +1
+        const addComment = [...feedData.replies, newComment];
+        // feedData 업데이트
+        setFeedData({
+          ...feedData,
+          repliesCount: addCommentCout,
+          replies: addComment,
+        });
+
+        setInputComment("");
+        // window.location.reload();
+      } else {
+        console.error("status code:", res?.status, res?.data);
+        toast.error("댓글을 추가할 수 없습니다.");
+      }
+    } catch (err) {
+      console.error("err comment:", err);
+      toast.error("댓글을 추가할 수 없습니다.");
+    }
+  };
+
+  // 엔터 누르면 자동으로 입력
+  const handleEnter = (e) => {
+    if (e.key === "Enter") {
+      postComment();
+    }
+  };
+
+
   return (
     <>
       {/* 댓글 수 표시 */}
@@ -15,19 +70,20 @@ const CommentInput = ({ feedData }) => {
       </div>
       <div className="flex w-full mt-4">
         <img
-          src="https://homepagepictures.s3.ap-northeast-2.amazonaws.com/client/public/images/userComment.png"
-          alt="유저사진"
+          src={profileImg} // 아직 유저 이미지 API에 없어서 일단 넣음..
+          alt="profileImg"
+          className="w-10 h-10 mr-2.5 rounded-full object-cover"
         />
         <div className="flex w-full relative">
           <input
             className="h-full w-full ml-4 border rounded-lg pl-4"
-            //   value={}
-            //   onChange={}
+            value={inputComment}
+            onKeyDown={handleEnter}
+            onChange={(e) => setInputComment(e.target.value)}
           />
           <button
             className="absolute right-0 top-1/4 pr-4"
-            //   onClick={() => postComment(comments.id)}
-          >
+            onClick={postComment}>
             입력
           </button>
         </div>
--- client/src/components/feed/comment/CommentOutput.jsx
+++ client/src/components/feed/comment/CommentOutput.jsx
@@ -1,78 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { toast } from "react-hot-toast";
+import api from "../../common/tokens";
 
-const CommentOutput = () => {
-    return (
-      <div>
-        {/* 댓글 수 표시 */}
-        <div className="flex">
-          <span className="text-xl font-semibold">댓글</span>
-          <span className="text-xl font-semibold text-[#35C5F0] ml-2">
-            댓글댓글댓글
-          </span>
-        </div>
-
-        {/* 댓글 출력창 */}
-        {/* {comments && */}
-          {/* comments.map((comment, index) => ( */}
-            <div
-                // key={comment.id}
-                className="flex flex-col mt-10">
-              <div className="bg-[#fceecd75] p-8 rounded-lg shadow">
-                <div className="flex items-start">
-                  <img
-                    src="https://homepagepictures.s3.ap-northeast-2.amazonaws.com/client/public/images/userComment.png"
-                    alt="유저사진"
-                  />
-                  <div className="flex flex-col ml-4 w-full">
-                    <span className="text-lg font-semibold">
-                      닉네임
-                    </span>
-                    <span className="my-4 text-base">
-                      ㅁㄴㅇㅁㄴㅇㅁㄴㅇ
-                    </span>
-                    {/* 작성날짜, 좋아요, 답글 */}
-                    <div className="flex items-center text-gray-500 font-medium text-base">
-                      {/* 작성날짜 */}
-                      <span className="mr-2.5">작성날짜</span>
-                      {/* 좋아요 */}
-                      {/* <button
-                        className="flex items-center"
-                        onClick={() => toggleLike(comment)}>
-                        <img
-                          className="w-6 h-6 mr-1"
-                          src={
-                            like[comment]
-                              ? "https://homepagepictures.s3.ap-northeast-2.amazonaws.com/client/public/images/heart-on.png"
-                              : "https://homepagepictures.s3.ap-northeast-2.amazonaws.com/client/public/images/heart-off.png"
-                          }
-                          alt="좋아요"
-                        />
-                        <span className="mr-1">좋아요</span>
-                      </button> */}
-                      {/* 답글 */}
-                      <button className="mx-2">답글 달기</button>
-                      {/* 수정 */}
-                      <button
-                        className="mx-2"
-                        // onClick={}
-                                  >
-                        수정하기
-                      </button>
-                      {/* 삭제 */}
-                      <button
-                        className="mx-2"
-                        // onClick={() => { }}
-                                  >
-                        삭제하기
-                      </button>
-                    </div>
+const CommentOutput = ({ feedData }) => {
+    const profileImg = localStorage.getItem("profileImg");
+    // 댓글 좋아요
+    const [like, setLike] = useState("");
+    // feedData 안에 댓글 접근
+    const [replies, setReplies] = useState("");
+    
+    useEffect(() => {
+        if (feedData) {
+            setReplies(feedData.replies);
+        }
+    }, [feedData]);
+    
+  return (
+    <div>
+      {/* 댓글 출력창 */}
+      {replies &&
+        replies.map((comment, index) => (
+          <div key={comment.id} className="flex flex-col mt-10">
+            <div className="bg-[#fceecd75] p-8 rounded-lg shadow">
+              <div className="flex items-start">
+                <img
+              src={profileImg}
+              alt="profileImg"
+              className='w-10 h-10 mr-2.5 rounded-full object-cover'
+            />
+                <div className="flex flex-col ml-4 w-full">
+                  <span className="text-lg font-semibold">
+                    {comment.nickname}
+                  </span>
+                  <span className="my-4 text-base">ㅁㄴㅇㅁㄴㅇㅁㄴㅇ</span>
+                  {/* 작성날짜, 좋아요, 답글 */}
+                  <div className="flex items-center text-gray-500 font-medium text-base">
+                    {/* 작성날짜 */}
+                    <span className="mr-2.5">{comment.createdDateTime.split("T")[0]}</span>
+                    {/* 좋아요 */}
+                    <button
+                      className="flex items-center"
+                      onClick={() => setLike(!like)}>
+                      <img
+                        className="w-6 h-6 mr-1"
+                        src={
+                          like
+                            ? "https://homepagepictures.s3.ap-northeast-2.amazonaws.com/client/public/images/heart-on.png"
+                            : "https://homepagepictures.s3.ap-northeast-2.amazonaws.com/client/public/images/heart-off.png"
+                        }
+                        alt="좋아요"
+                      />
+                      <span className="mr-1">좋아요</span>
+                    </button>
+                    {/* 답글 */}
+                    <button className="mx-2">답글 달기</button>
+                    {/* 수정 */}
+                    <button
+                      className="mx-2"
+                      // onClick={}
+                    >
+                      수정하기
+                    </button>
+                    {/* 삭제 */}
+                    <button
+                      className="mx-2"
+                      // onClick={() => { }}
+                    >
+                      삭제하기
+                    </button>
                   </div>
                 </div>
               </div>
             </div>
-          {/* ))} */}
-      </div>
-    );
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default CommentOutput;
