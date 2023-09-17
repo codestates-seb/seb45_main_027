ecb9f2896c0ab9c53d70c8c389226c6702535fdf
WONHO22 <dnjsgh2122@gmail.com>
2023-09-17 22:51:03 +0900

    [Feat] #185 - 로그인 성공시 이전 요청했었던 경로로 redirect 해주는 기능 구현

--- client/src/pages/ProtectedRoute.jsx
+++ client/src/pages/ProtectedRoute.jsx
@@ -1,10 +1,15 @@
-import { Navigate } from "react-router-dom";
+import React from "react";
+import { Route, Routes, useLocation, Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
   const user = localStorage.getItem("memberId");
+  const location = useLocation();
+
   if (!user) {
+    localStorage.setItem("prevPath", location.pathname); // 이전 경로 저장
     return <Navigate to="/login" />;
   }
+
   return children;
 };
 
