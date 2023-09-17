045204a6a26e6e10a31ab5a517f3768872063872
yebin <yebind3308@gmail.com>
2023-09-18 07:30:36 +0900

    [Design] #13 - 메인화면 슬로건, 스크롤 수정

--- client/src/components/main/MainSlogan.jsx
+++ client/src/components/main/MainSlogan.jsx
@@ -5,22 +5,18 @@ const MainSlogan = () => {
       <div className="flex md:hidden bg-[#FFFAEE] h-20 w-full  items-center overflow-hidden">
         <div className="h-full w-full animate-slogan font-extrabold text-3xl flex shadow-2xl">
           <img
-            className=""
             src="https://homepagepictures.s3.ap-northeast-2.amazonaws.com/client/public/images/slogan.png"
             alt="인테리어 추천 커뮤니티"
           />
           <img
-            className=""
             src="https://homepagepictures.s3.ap-northeast-2.amazonaws.com/client/public/images/slogan.png"
             alt="인테리어 추천 커뮤니티"
           />
           <img
-            className=""
             src="https://homepagepictures.s3.ap-northeast-2.amazonaws.com/client/public/images/slogan.png"
             alt="인테리어 추천 커뮤니티"
           />
           <img
-            className=""
             src="https://homepagepictures.s3.ap-northeast-2.amazonaws.com/client/public/images/slogan.png"
             alt="인테리어 추천 커뮤니티"
           />
