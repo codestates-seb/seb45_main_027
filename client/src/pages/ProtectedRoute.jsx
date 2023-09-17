e36e03d627ea4774c7eaef1b7a371b2579ec6371
WONHO22 <dnjsgh2122@gmail.com>
2023-09-17 23:05:54 +0900

    [Feat] #185 - 비회원 로그인페이지 route 시 toast 알람 추가

--- client/src/pages/ProtectedRoute.jsx
+++ client/src/pages/ProtectedRoute.jsx
@@ -1,5 +1,20 @@
 import React from "react";
-import { Route, Routes, useLocation, Navigate } from "react-router-dom";
+import { useLocation, Navigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+const toastStyle = {
+  style: {
+    border: "1px solid #b91604",
+    padding: "8px",
+    color: "#b91604",
+    fontSize: "14px",
+  },
+  iconTheme: {
+    primary: "#b91604",
+    secondary: "#FFFAEE",
+  },
+  duration: 2000,
+};
 
 const ProtectedRoute = ({ children }) => {
   const user = localStorage.getItem("memberId");
@@ -7,6 +22,7 @@ const ProtectedRoute = ({ children }) => {
 
   if (!user) {
     localStorage.setItem("prevPath", location.pathname); // 이전 경로 저장
+    toast.error("로그인이 필요한 서비스입니다.", toastStyle);
     return <Navigate to="/login" />;
   }
 
