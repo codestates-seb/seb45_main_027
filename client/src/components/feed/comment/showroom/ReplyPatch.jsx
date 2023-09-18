2f7794f889fa8d758f8d96e51ac5a64c8b9ddc17
WONHO22 <dnjsgh2122@gmail.com>
2023-09-19 02:38:00 +0900

    [Fix] #176 - 답글 수정 및 삭제시 정상적으로 상태변화가 이루어지지 않는 버그 수정

--- client/src/components/feed/comment/showroom/ReplyPatch.jsx
+++ client/src/components/feed/comment/showroom/ReplyPatch.jsx
@@ -15,6 +15,7 @@ const ReplyPatch = ({
   const commentId = comment.feedCommentId;
   const feedCommentId = comment.feedCommentId;
   const [newContent, setNewContent] = useState(comment.content); // 수정하기를 누를때 기존 값 유지하기 위해서!
+  const [afterContent, setAfterContent] = useState(""); // 수정후 결과값
 
   // PATCH 요청
   const patchReply = async () => {
@@ -33,20 +34,22 @@ const ReplyPatch = ({
       const response = await api(configParams);
 
       if (response && response.status === 200) {
-        // feedData를 복사해서 업데이트
-        const updatedFeedData = JSON.parse(JSON.stringify(feedData));
+        // feedData를 얕은복사
+        const updatedFeedData = { ...feedData };
 
         updatedFeedData.replies.forEach((reply) => {
           if (reply.feedCommentId === feedCommentId) {
-            reply.comments = reply.comments.map((cmt) => {
-              if (cmt.feedCommentId === commentId) {
-                return { ...cmt, content: newContent };
+            reply.comments = reply.comments.map((comment) => {
+              if (comment.feedCommentId === commentId) {
+                // 새로운 객체 생성하여 내용 수정
+                return { ...comment, content: newContent };
               }
-              return cmt;
+              return comment;
             });
           }
         });
         setFeedData(updatedFeedData);
+        setAfterContent(newContent);
         toast.success("답글이 수정되었습니다.");
       } else {
         toast.error("답글을 수정하지 못했습니다. 다시 시도해 주세요.");
@@ -60,6 +63,10 @@ const ReplyPatch = ({
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
       patchReply();
+      setEditReply({
+        ...editReply,
+        [comment.feedCommentId]: false,
+      });
     }
 
     if (e.key === "Escape") {
@@ -103,6 +110,8 @@ const ReplyPatch = ({
             </button>
           </div>
         </>
+      ) : afterContent ? (
+        afterContent
       ) : (
         comment.content
       )}
