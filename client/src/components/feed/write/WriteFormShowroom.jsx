ab3b2317a06b68141ba18f0765aacd282e3d5f7e
yebin <yebind3308@gmail.com>
2023-09-21 05:27:00 +0900

    [Design] #247 - showRoom 작성찰 레이아웃/디자인 수정

--- client/src/components/feed/write/WriteFormShowroom.jsx
+++ client/src/components/feed/write/WriteFormShowroom.jsx
@@ -22,6 +22,10 @@ const WriteFormShowroom = ({
   const [tags, setTags] = useState([]);
   const [currentTag, setCurrentTag] = useState({ x: "0%", y: "0%", text: "" });
 
+  // 버튼효과
+  const buttonStyle =
+    "py-2 px-4 border-[1px] mx-2 rounded-md hover:bg-gray-200 hover:bg-opacity-50";
+
   useEffect(() => {
     if (editorContent === DEFAULT_EDITOR_TEXT) {
       editorRef.current.innerHTML = editorContent;
@@ -131,35 +135,34 @@ const WriteFormShowroom = ({
         </button>
 
         {/* 구분선 */}
-        <div className="border-r-[1px] h-10 my-auto mx-3"></div>
+        <div className="border-r-[1px] h-10 my-auto mx-3 "></div>
         <button
           onClick={() => {
             toggleStyle("bold");
           }}
-          className={`py-2 px-4 border-[1px] mx-2 rounded-md`}>
+          className={buttonStyle}>
           <FaBold />
         </button>
-        <button
-          onClick={() => toggleStyle("italic")}
-          className={`py-2 px-4 border-[1px] mx-2 rounded-md`}>
+        <button onClick={() => toggleStyle("italic")} className={buttonStyle}>
           <FaItalic />
         </button>
         <button
           onClick={() => toggleStyle("underline")}
-          className={`py-2 px-4 border-[1px] mx-2 rounded-md`}>
+          className={buttonStyle}>
           <FaUnderline />
         </button>
         <button
           onClick={() => toggleStyle("strikethrough")}
-          className={`py-2 px-4 border-[1px] mx-2 rounded-md`}>
+          className={buttonStyle}>
           <FaStrikethrough />
         </button>
       </div>
 
       <div className="flex-col justify-center content-center">
-        <div className="m-4 p-3 flex flex-col border-[3px] rounded-md">
+        <div className="m-4 p-10 flex flex-col border shadow rounded-xl bg-[#F5634A] bg-opacity-10">
           <ImageEditGuide />
-          {imageSrc && <div>이미지 편집기</div>}
+          {/* {imageSrc && <div>이미지 편집기</div>} */}
+
           <PhotoTagging
             imageSrc={imageSrc}
             tags={tags}
@@ -168,11 +171,13 @@ const WriteFormShowroom = ({
             setCurrentTag={setCurrentTag}
           />
           {imageSrc && (
-            <div className="flex justify-center mt-2">
+            <div className="w-full flex justify-center mt-10">
               <button
                 onClick={handlePost}
-                className="p-2 border-[1px] mx-2 w-1/8 rounded-md">
-                이미지 등록
+                className="flex items-center bg-[#F5634A] bg-opacity-40 hover:bg-opacity-20 mx-4 px-5 py-2 rounded-md shadow">
+                <span className="px-4 py-2 text-xl font-semibold text-white">
+                  이미지 등록하기
+                </span>
               </button>
             </div>
           )}
@@ -183,7 +188,7 @@ const WriteFormShowroom = ({
           dangerouslySetInnerHTML={{ __html: editorContent }}
           onFocus={handleFocus}
           onBlur={handleBlur}
-          className="p-2 m-4 h-full  min-h-[600px] text-md border-[3px] rounded-md"></div>
+          className="p-10 m-4 h-full border min-h-[600px] text-md rounded-md shadow text-xl focus:outline-none focus:shadow-xl"></div>
       </div>
     </>
   );
