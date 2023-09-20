5c8c6e93716c125999940dada0393ccb9e7118b7
yebin <yebind3308@gmail.com>
2023-09-20 21:48:58 +0900

    [Fix] #247 - 페이지 배경색상 그라데이션 적용

--- client/src/components/common/Background.jsx
+++ client/src/components/common/Background.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Background = ({ mainclassName, divclassName, style, children }) => {
   return (
     <main
-      className={`flex justify-center w-full min-h-screen pt-0 md:pt-24 ${mainclassName}`}
+      className={`flex justify-center w-full min-h-screen pt-0 md:pt-24 bg-gradient-to-b from-[#FFFAEE] via-[#fff1d1] to-[#FFFAEE] ${mainclassName}`}
       style={style}>
       <div className={`flex h-full w-full max-w-[1440px] ${divclassName}`}>
         {children}
