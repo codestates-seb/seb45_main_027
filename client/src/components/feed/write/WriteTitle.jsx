4a3e4a962fdadcc2d7453b22bdcb84c44497e256
WONHO22 <dnjsgh2122@gmail.com>
2023-09-25 22:19:39 +0900

    [Fix] #171, #182 - 모바일 글작성 시 글 제목 font-size 조정

--- client/src/components/feed/write/WriteTitle.jsx
+++ client/src/components/feed/write/WriteTitle.jsx
@@ -12,7 +12,7 @@ const WriteTitle = ({ title, setTitle }) => {
         value={title}
         onChange={TitleChange}
         placeholder="제목을 입력해주세요."
-        className="px-5 my-2 py-2.5 w-full text-3xl font-semibold text-gray-800 "
+        className="px-5 my-2 py-2.5 w-full text-xl sm:text-3xl font-semibold text-gray-800 "
       />
     </div>
   );
